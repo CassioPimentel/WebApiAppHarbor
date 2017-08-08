c53702dcb5b1c382dad44a27c8404ee121244cdc
Cassio Alves Pimentel <CassioPimentel@odilonsantos.com>
2017-08-08 14:31:03 -0300

    Add project files.

---
+++ WebAPIappHabor/Models/Funcionario.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPIappHabor.Models
+{
+    [Table("dbfe08b01ebb344c598f92a7c800c6b3d6.Funcionario")]
+    public class Funcionario
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
