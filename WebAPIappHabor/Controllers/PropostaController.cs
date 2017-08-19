35d952353304231dc5260a745ef8798e7471940f
Cassio Alves <cassio.999@hotmail.com>
2017-08-19 09:46:55 -0300

    teste

--- WebAPIappHabor/Controllers/PropostaController.cs
+++ WebAPIappHabor/Controllers/PropostaController.cs
@@ -22,6 +22,7 @@ namespace WebAPIappHabor.Controllers
             return db.Proposta.Include("Profissional").Include("Empresa");
         }
 
+
         [Route("GetPropostaPorProfissional")]
         [ResponseType(typeof(Proposta))]
         public IHttpActionResult GetPropostaPorProfissional(int id)
